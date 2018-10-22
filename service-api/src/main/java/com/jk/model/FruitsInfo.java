7e3c8e222e857542adc88b8a9121085b9bb55282
xuech <1632917013@qq.com>
2018-10-22 11:42:48 +0800

    提交实体类

--- service-api/src/main/java/com/jk/model/FruitsInfo.java
+++ service-api/src/main/java/com/jk/model/FruitsInfo.java
@@ -23,6 +23,25 @@ public class FruitsInfo implements Serializable {
 
     private Integer typeId;
 
+    /*
+     *  薛长欢
+     *
+     *  类型业务字段
+     */
+    private String typeName;
+
+    private String placeName;
+
+    private String img1;
+
+    private String img2;
+
+    private String img3;
+
+    private String img4;//业务字段end
+
+    private Integer amount;//数量
+
     public Integer getId() {
         return id;
     }
@@ -95,6 +114,62 @@ public class FruitsInfo implements Serializable {
         this.typeId = typeId;
     }
 
+    public String getTypeName() {
+        return typeName;
+    }
+
+    public void setTypeName(String typeName) {
+        this.typeName = typeName;
+    }
+
+    public String getImg1() {
+        return img1;
+    }
+
+    public void setImg1(String img1) {
+        this.img1 = img1;
+    }
+
+    public String getImg2() {
+        return img2;
+    }
+
+    public void setImg2(String img2) {
+        this.img2 = img2;
+    }
+
+    public String getImg3() {
+        return img3;
+    }
+
+    public void setImg3(String img3) {
+        this.img3 = img3;
+    }
+
+    public String getImg4() {
+        return img4;
+    }
+
+    public void setImg4(String img4) {
+        this.img4 = img4;
+    }
+
+    public String getPlaceName() {
+        return placeName;
+    }
+
+    public void setPlaceName(String placeName) {
+        this.placeName = placeName;
+    }
+
+    public Integer getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Integer amount) {
+        this.amount = amount;
+    }
+
     @Override
     public String toString() {
         return "FruitsInfo{" +
@@ -107,6 +182,13 @@ public class FruitsInfo implements Serializable {
                 ", num=" + num +
                 ", price=" + price +
                 ", typeId=" + typeId +
+                ", typeName='" + typeName + '\'' +
+                ", placeName='" + placeName + '\'' +
+                ", img1='" + img1 + '\'' +
+                ", img2='" + img2 + '\'' +
+                ", img3='" + img3 + '\'' +
+                ", img4='" + img4 + '\'' +
+                ", amount=" + amount +
                 '}';
     }
 }
