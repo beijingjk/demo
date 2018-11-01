b1fd46f335d7724cfd40830a37ac978178009b81
wangt <228385317@qq.com>
2018-11-01 11:28:31 +0800

    666

--- springcloud-eureka-client/src/main/java/com/jk/service/personal/PersonalServiceImpl.java
+++ springcloud-eureka-client/src/main/java/com/jk/service/personal/PersonalServiceImpl.java
@@ -1,4 +1,26 @@
 package com.jk.service.personal;
 
+import com.jk.mapper.personal.PersonalMapper;
+import com.jk.model.Login;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Service
 public class PersonalServiceImpl implements PersonalServiceApi{
+
+    @Autowired
+    private PersonalMapper personalMapper;
+
+    @Override
+    public void addUser(Login login) {
+        personalMapper.addUser(login);
+    }
+
+    @Override
+    public void updPassword(Login login,String id) {
+
+        personalMapper.updPassword(login,id);
+    }
 }
