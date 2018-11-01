076e900ff7d4ca523be0486755b6abf4aa898530
wangt <228385317@qq.com>
2018-11-01 11:06:33 +0800

    666

--- springcloud-eureka-client/src/main/java/com/jk/controller/personal/PersonalController.java
+++ springcloud-eureka-client/src/main/java/com/jk/controller/personal/PersonalController.java
@@ -1,10 +1,38 @@
 package com.jk.controller.personal;
 
+import com.jk.model.Login;
 import com.jk.service.personal.PersonalServiceApi;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.servlet.http.HttpServletRequest;
+
+@RequestMapping("personal")
+@Controller
 public class PersonalController {
 
     @Autowired
     private PersonalServiceApi personalService;
+
+    @RequestMapping("addLogin")
+    @ResponseBody
+    public void addLogin(@RequestBody Login login){
+        personalService.addUser(login);
+    }
+
+    @RequestMapping("updPassword")
+    @ResponseBody
+    public void updPassword(Login login, HttpServletRequest request){
+
+        Login user= (Login) request.getSession().getAttribute("login");
+
+        String id=user.getUserId();
+
+        personalService.updPassword(login,id);
+    }
+
 }
