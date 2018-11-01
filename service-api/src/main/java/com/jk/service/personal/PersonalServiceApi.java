9bb10e6240e3a14ee23cbc851da9dffd43b77dde
wangt <228385317@qq.com>
2018-11-01 11:31:21 +0800

    888666

--- service-api/src/main/java/com/jk/service/personal/PersonalServiceApi.java
+++ service-api/src/main/java/com/jk/service/personal/PersonalServiceApi.java
@@ -1,5 +1,16 @@
 package com.jk.service.personal;
 
+import com.jk.model.Login;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
 public interface PersonalServiceApi {
 
+    @RequestMapping(value = "personal/addLogin",method = RequestMethod.POST)
+    void addUser(@RequestBody Login login);
+
+    @RequestMapping(value = "personal/updPassword",method = RequestMethod.GET)
+    void updPassword(@RequestBody Login login,@RequestParam("id") String id);
 }
